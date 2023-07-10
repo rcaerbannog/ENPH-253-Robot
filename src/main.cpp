f211b2823b094a3b9b5bfd5a5e6eaa699fd1c31a
alexanderli-laptop <alexanderli.work@gmail.com>
2023-07-09 23:11:26 -0700

    Added rudimentary line-following code to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <Arduino.h>
+#include <Servo.h>
 
 // PIN DEFINITIONS
-#define PIN_TAPE_SENSOR_LEFT PA0
-#define PIN_TAPE_SENSOR_RIGHT PA1
+#define PIN_TAPE_SENSOR_LEFT PA4	// as analog voltage reading
+#define PIN_TAPE_SENSOR_RIGHT PA5	// as analog voltage reading
+#define PIN_STEERING_SERVO PA11	// as PWM servo control
+#define PIN_LMOTOR_FWD PB6
+#define PIN_LMOTOR_REV PB7
+#define PIN_RMOTOR_FWD PB8
+#define PIN_RMOTOR_REV PB9
+
+
 
 // ENUMS
 
 
 // CONSTANTS
-
+const int TAPE_SENSOR_THRESHOLD = 500;
 
 
 // VARIABLES
@@ -19,42 +27,76 @@ bool leftOnTape;
 int tapePosErr; // between -2 and +2
 int tapePosErrPrev;
 
+// Motor control
+int motorDutyCycle = 200; 	// duty cycle fraction out of 255. Manual setting for testing only.
+
 
 // put function declarations here:
-int myFunction(int, int);
-void updateTapeSensor();
+void updateTapeError();
 void steering();
 void motorControl();
+
+Servo steeringServo;	// might replace with manual PWM control. Test duty cycle to servo angle map with calibration program.
 
 void setup() {
   pinMode(PIN_TAPE_SENSOR_LEFT, INPUT_PULLUP);
   pinMode(PIN_TAPE_SENSOR_RIGHT, INPUT_PULLUP);
-  int result = myFunction(2, 3);
+  
+  pinMode(PIN_LMOTOR_FWD, OUTPUT);
+  pinMode(PIN_LMOTOR_REV, OUTPUT);
+  pinMode(PIN_RMOTOR_FWD, OUTPUT);
+  pinMode(PIN_RMOTOR_REV, OUTPUT);
+  
+  //pinMode(PIN_STEERING_SERVO, OUTPUT);
+  steeringServo.attach(PIN_STEERING_SERVO);
+  
+  // for testing, add an assertions check to make sure all variables remain in range. (Basically implementing a rep invariant checker.)
+  // default testing:
+  analogWrite(PIN_LMOTOR_FWD, motorDutyCycle);
+  analogWrite(PIN_RMOTOR_FWD, motorDutyCycle);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-
-void updateTapeSensor() {
-  
+  tapeFollowing();
+  delay(100);	// replace with clock absolute reference
 }
 
 /*
- * PID
+ * Tape following error determination from reflectance sensors
+ * We read the voltage from BEFORE the phototransistor, so high voltage means low reflectance, and on tape
+ * Negative error means we are to the left, and positive error to the right
  */
-void motorControl() {
-
+void updateTapeError() {
+  int leftTapeSensorValue = analogRead(PIN_TAPE_SENSOR_LEFT);
+  int rightTapeSensorValue = analogRead(PIN_TAPE_SENSOR_RIGHT);
+  boolean leftOnTape = leftTapeSensorValue > TAPE_SENSOR_THRESHOLD;
+  boolean rightOnTape = rightTapeSensorValue > TAPE_SENSOR_THRESHOLD;
+  if (leftOnTape && rightOnTape) {
+	tapePosErr = 0;  
+  } else if (leftOnTape) {
+	tapePosErr = -1;
+  } else if (rightOnTape) {
+	tapePosErr = 1;
+  } else if (tapePosErrPrev) {
+	tapePosErr = -2;
+  } else {
+	tapePosErr = 2;
+  }
+  /* Analog reading: if both sensors read something, fuzz steering angle to difference between sensor readings
+  Lower threshold for sensors above which we consider difference
+  Otherwise just the maximum angle
+  */
+  tapePosErrPrev = tapePosErr;
 }
 
 /*
- * PID
-*/
-void steering() {
-
+ * Non-PID for now
+ * Assume for now that we want to go at full speed all the time, no power differences
+ */
+void tapeFollowing() {
+	// Adjust steering to avoid twitching on turns, or is what we have fine? (May use current steering position as reference, or maybe perform smoothing. Google?)
+	int steering_angle = 10 * tapePosError;	// degrees
+	int servo_pos = 90 + steering_angle;
+	steeringServo.write(servo_pos);	
 }
+
